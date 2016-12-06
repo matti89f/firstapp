def2f761032dd2a33431d96b36bf02bcb5c10c96
matti89f <matti.software@hotmail.com>
2016-12-06 20:25:27 +0100

    Add some validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,8 @@
 class Quote < ActiveRecord::Base
-  validates :saying, presence: true, length: { maximum:140, minimum:3 }
-  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :saying, presence: true,
+                     length: { maximum:140, minimum:3 }
+  validates :author, presence: true,
+                     length: { maximum: 50, minimum: 3 }
+  validates :author, :saying, format: { without: /fuck/ }
+  validates :author, :saying, format: { without: /ass/ }
 end
